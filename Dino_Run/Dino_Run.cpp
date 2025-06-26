4a3edfb23c6809a93349190a33c57567e0173334
Jsd1573 <longlive18@naver.com>
2025-06-26 15:48:09 +0900

    (25.06.26)

--- Dino_Run/Dino_Run.cpp
+++ Dino_Run/Dino_Run.cpp
@@ -3,359 +3,391 @@
 #include <windows.h>
 #include <string.h>
 
-#define UP 0
-#define DOWN 1
-#define LEFT 2
-#define RIGHT 3
-#define SUBMIT 4
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+#define SUBMIT 32
 
 using namespace std;
 
-char tempmap[10][10];
-
-char map1[10][10] =
+void gotoxy(int x, int y)
 {
-	{'1','1','1','1','1','1','1','1','1','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','3','0','0','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','2','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','1','1','1','1','1','1','1','1','1'}
-};
-char map2[10][10] =
-{
-	{'1','1','1','1','1','1','1','1','1','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','3','0','0','0','0','3','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','2','0','0','0','0','1'},
-	{'1','0','0','0','0','0','0','0','0','1'},
-	{'1','0','0','0','3','0','0','0','0','1'},
-	{'1','1','1','1','1','1','1','1','1','1'}
-};
-
-void HideCursor()
-{
-	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO cursorInfo;
-	GetConsoleCursorInfo(consoleHandle, &cursorInfo);
-	cursorInfo.bVisible = FALSE;
-	SetConsoleCursorInfo(consoleHandle, &cursorInfo);
+    COORD pos = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void gotoxy(int x, int y) // 커서 위치 옮기는 함수
+int Keycontrol()
 {
-	COORD POS = { x ,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), POS);
+    int temp = _getch();
 
-}
+    if (temp == 224)
+    {
+        temp = _getch();
 
-int KeyControl()
-{
-	 char temp = _getch();
-	 
-	 if (temp == 'w')
-	 {
-		 return UP;  // 0
-	 }
-	 else if (temp == 's')
-	 {
-		 return DOWN;  // 1
-	 }
-	 else if (temp == 'a')
-	 {
-		 return LEFT;  // 2
-	 }
-	 else if (temp == 'd')
-	 {
-		 return RIGHT; // 3
-	 }
-	 else if (temp == ' ')
-	 {
-		 return SUBMIT; // 4
-	 }
+        if (temp == 72)
+        {
+            return UP;
+        }
+        else if (temp == 80)
+        {
+            return DOWN;
+        }
+        else if (temp == 75)
+        {
+            return LEFT;
+        }
+        else if (temp == 77)
+        {
+            return RIGHT;
+        }
+        else if (temp == 32)
+        {
+            return SUBMIT;
+        }
+    }
+
 }
 
 void TitleDraw()
 {
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	printf("	###    #  ##     #     ##         ####     #    #  ##     # \n");
-	printf("	#  #   #  # #    #    #  #        #   #    #    #  # #    # \n");
-	printf("	#   #  #  #  #   #   #    #       #    #   #    #  #  #   # \n");
-	printf("	#   #  #  #   #  #   #    #       #####    #    #  #   #  # \n");
-	printf("	#  #   #  #    # #    #  #        #    #   #    #  #    # # \n");
-	printf("	###    #  #     ##     ##         #     #    ##    #     ## \n");
-	cout << endl;
-	cout << endl;
-	cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    printf("	###    #  ##     #     ##         ####     #    #  ##     # \n");
+    printf("	#  #   #  # #    #    #  #        #   #    #    #  # #    # \n");
+    printf("	#   #  #  #  #   #   #    #       #    #   #    #  #  #   # \n");
+    printf("	#   #  #  #   #  #   #    #       #####    #    #  #   #  # \n");
+    printf("	#  #   #  #    # #    #  #        #    #   #    #  #    # # \n");
+    printf("	###    #  #     ##     ##         #     #    ##    #     ## \n");
+    cout << endl;
+    cout << endl;
+    cout << endl;
 
 }
 
-int MenuDraw()
+
+
+class Maze
 {
-	int x = 34;
-	int y = 12;
+public:
+    static const int rows = 10;
+    static const int cols = 15;
 
-	gotoxy(x-2, y);
-	cout << "> 게임 시작";
-	gotoxy(x, y+1);
-	cout << "게임 정보";
-	gotoxy(x, y+2);
-	cout << "게임 종료";
+    char map1[rows][cols + 1] =
+    {
+        "###############",
+        "#      #     E#",
+        "# ### ##### ###",
+        "#   #     #   #",
+        "### # ### ### #",
+        "#   #   #     #",
+        "# ##### ##### #",
+        "#     #     # #",
+        "#A### ##### # #",
+        "###############"
+    };
 
-	while(1)
-	{
-		int n = KeyControl();
+    static const int rows2 = 20;
+    static const int cols2 = 25;
 
-			switch (n)
-			{
-				case UP :
-				{
-					if (y >12)
-					{
-						gotoxy(x - 2, y);
-						printf(" ");
-						gotoxy(x - 2, --y);
-						printf(">");
-					}
-					break;
-				}
-				case DOWN :
-				{
-					if (y < 14)
-					{
-						gotoxy(x - 2, y);
-						printf(" ");
-						gotoxy(x - 2, ++y);
-						printf(">");
-					}
-					break;
-				}
+    char map2[rows2][cols2 + 1] =
+    {
+        "#########################",
+        "#      #     E###########",
+        "# ### ##### #############",
+        "#   #     #   ###########",
+        "##### ### ### ###########",
+        "#   #   #     ###########",
+        "# ##### ##### ###########",
+        "#     #     # ###########",
+        "# ### ##### # ###########",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################",
+        "#########################"
+    };
 
-				case SUBMIT:
-				{
-					return y - 12;
-				}
-			}
-	}
-}
 
-void InfoDraw()
+    void draw(int x)
+    {
+        switch (x)
+        {
+        case 1: for (int i = 0; i < rows; ++i)
+        {
+            gotoxy(0, i);
+            cout << map1[i];
+        }
+              break;
+
+        case 2: for (int i = 0; i < rows2; ++i)
+        {
+
+            gotoxy(0, i);
+            cout << map2[i];
+        }
+              break;
+
+        }
+    }
+};
+
+class Player
 {
-	system("cls");
-	printf("\n\n\n\n\n\n");
-	printf("                                   [조작법]\n\n");
-	printf("                              이동 : W, A, S, D\n");
-	printf("                              선택 : 스페이스바 \n\n");
-	printf("                  스페이스바를 누르면 메인화면으로 이동합니다.");
-	while(1)
-	{
-		if (KeyControl() == SUBMIT)
-		{
-			break;								// infodraw함수에 while 문을 적지않으면 
-		}										// 메뉴에서 infodraw를 호출해도 거의 바로 즉시 다시 메뉴 화면으로 돌아가게 되던데 
-	}											// 왜 이런지 그리고 왜 while문을 적으면 바로 즉시 메뉴화면으로 돌아가지않고 infodraw함수로 호출한 화면이 나와서 유지되는지
+public:
+    int x = 1;              //Player 시작 지점
+    int y = 1;
+
+    void draw(Maze& maze, int x)
+    {
+        switch (x)
+        {
+        case 1: maze.map1[y][x] = 'p';
+            gotoxy(x, y);
+            cout << 'p';
+            break;
+
+        case 2: maze.map2[y][x] = 'p';
+            gotoxy(x, y);
+            cout << 'p';
+            break;
+        }
+    }
+
+    int move(Maze& maze, int dx, int dy, int s)         // Maze 클래스의 maze 객체를 매개변수로 넣음으로써 maze객체의 정보를 한꺼번에 함수에 전달
+    {
+        int newX = x + dx;
+        int newY = y + dy;
+        char next;
+
+        switch (s)
+        {
+        case 1: next = maze.map1[newY][newX];
+
+            if (next == '#')
+            {
+                return 0;           // if문은 조건이 참일경우 에만 내용을 실행  조건이 거짓일 경우 if문은 건너뛰고 다음 문장 실행
+            }
+
+            if (next == 'A')
+            {
+                gotoxy(x, y); cout << ' ';
+                maze.map1[y][x] = ' ';
+                x = newX;
+                y = newY;
+                maze.map1[y][x] = 'P';
+                gotoxy(x, y); cout << 'P';
+
+                return 1;
+            }
+
+            if (next == 'E')
+            {
+                return 2;
+            }
+
+            gotoxy(x, y); cout << ' ';
+            maze.map1[y][x] = ' ';
+            x = newX;
+            y = newY;
+            maze.map1[y][x] = 'P';
+            gotoxy(x, y); cout << 'P';
+
+        case 2: next = maze.map2[newY][newX];
+
+            if (next == '#')
+            {
+                return 0;           // if문은 조건이 참일경우 에만 내용을 실행  조건이 거짓일 경우 if문은 건너뛰고 다음 문장 실행
+            }
+
+            if (next == 'A')
+            {
+                gotoxy(x, y); cout << ' ';
+                maze.map2[y][x] = ' ';
+                x = newX;
+                y = newY;
+                maze.map2[y][x] = 'P';
+                gotoxy(x, y); cout << 'P';
+
+                return 1;
+            }
+
+            if (next == 'E')
+            {
+                return 2;
+            }
+
+            gotoxy(x, y); cout << ' ';
+            maze.map2[y][x] = ' ';
+            x = newX;
+            y = newY;
+            maze.map2[y][x] = 'P';
+            gotoxy(x, y); cout << 'P';
+        }
 
 
-}
+        // return 0;
+    }
 
-void MapDraw(int *x, int *y)
+};
+
+class Menu
 {
-	system("cls");
+public:
+    void InfoDraw()
+    {
+        system("cls");
+        printf("\n\n\n\n\n\n");
+        printf("                                   [조작법]\n\n");
+        printf("                              이동 : W, A, S, D\n");
+        printf("                              선택 : 스페이스바 \n\n");
+        printf("                  스페이스바를 누르면 메인화면으로 이동합니다.");
 
-	int i, j; // 세로 가로
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			char temp = tempmap[i][j];
+        while (1)
+        {
+            if (Keycontrol() == SUBMIT)
+            {
+                system("cls");
+                break;
+            }
+        }
+    }
+    int draw(int x, int y)
+    {
+        gotoxy(x - 2, y);
+        cout << "> 게임 시작";
+        gotoxy(x, y + 1);
+        cout << "게임 정보";
+        gotoxy(x, y + 2);
+        cout << "게임 종료";
 
-			if (temp == '0')  // 공백
-			{
-				cout << "  ";
-			}
-			else if (temp == '1')  // 벽
-			{
-				cout << "■ ";
-			}
-			else if (temp == '2')  // 플레이어 
-			{
-				cout << "☆ ";
-				*x = j;
-				*y = i;
-				
-			}
-			else if (temp == '3')  // 박스
-			{
-				cout << "□ ";
-			}
-			
-		}
-		cout << endl;
-	}
-	// Sleep(3000);
-	
-}
+        while (1)
+        {
+            int n = Keycontrol();
 
- void Move(int * x, int * y, int _x, int _y)
- {
-	 char mapobject = tempmap[*y + _y][*x + _x];
-	
-	 if (mapobject == '0')
-	 {
-		 gotoxy(*x, *y);
-		 printf("  ");
+            switch (n)
+            {
+            case UP:
+                if (y > 12)
+                {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, --y);
+                    printf(">");
+                }
+                break;
 
-		 gotoxy(*x + _x, *y + _y);
-		 printf("☆");
+            case DOWN:
+                if (y < 14)
+                {
+                    gotoxy(x - 2, y);
+                    printf(" ");
+                    gotoxy(x - 2, ++y);
+                    printf(">");
+                }
+                break;
 
-		 *x = *x + _x;
-		 *y = *y + _y;
-	 }
+            case SUBMIT: return y - 12;
 
-	 
- }
+            }
+        }
 
-int maplistdraw()
+
+
+    }
+};
+
+// ------------------- Game 클래스 -------------------
+class Game
 {
-	int x = 34;
-	int y = 12;
+public:
+    Maze maze;
+    Player player;
+    Menu menu;
+    int running = 1;
 
-	system("cls");
-	printf("\n\n\n\n\n\n\n\n\n");
-	printf("                                 [맵선택] \n\n");
+    void run()
+    {
 
-	gotoxy(x - 2, y);
-	printf("> 1 단계");
-	gotoxy(x, y + 1);
-	printf("2 단계");
-	gotoxy(x, y + 2);
-	printf(" 뒤로");
+        while (1)
+        {
+            TitleDraw();
+            int menucode = menu.draw(34, 12);
 
-	while (1)
-	{
-		int n = KeyControl();
-		switch (n)
-		{
-			case UP:
-			{
-				if (y > 12)
-				{
-				gotoxy(x - 2, y);
-				printf(" ");
-				gotoxy(x - 2, --y);
-				printf(">");
-				}
-				break;
-			}
-			case DOWN:
-			{
-				if (y < 14)
-				{
-					gotoxy(x - 2, y);
-					printf(" ");
-					gotoxy(x - 2, ++y);
-					printf(">");
-				}
-				break;
-			}
-			case SUBMIT:
-			{
-				return y - 12;
-			}
+            if (menucode == 0)  // 게임 시작
+            {
+                break;      // break 문은 반복문(for, while등등)이나 switch문을 즉시 종료시키는데 사용되는 제어문
+            }                 // break 가 실행되면 ,해당 블록을 빠져나와 다음 코드로 흐름이 이동, break 문은 가장 가까운 반복문이나 switch문을 탈출
+            else if (menucode == 1)     // 게임 정보
+            {
+                menu.InfoDraw();
+            }
+            else if (menucode == 2)     // 게임 종료
+            {
+                return;         // return문의 기능에는 값을 반환하는것도 있지만 자신이 속해있는 '함수를' 종료시키는 기능도 있습니다.
+            }
+        }
+        // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-		}
-	}
-}
+        system("cls");              // 기존에 출력되어 있던 타이틀이나 메뉴 를 전부 정리해주기위해 사용
+        maze.draw(2);            // maze객체의 draw함수를 호출
+        player.draw(maze, 2);      // player객체의 draw함수를 호출하면서 maze객체를 인수로 전달
 
-void Gloop(int mapcode)
-{
-	int x = 0;
-	int y = 0;
+        int win = 0;
 
-	int playing = 1;
+        int clear = 0;
 
-	if (mapcode == 0)
-	{
-		memcpy(tempmap, map1, sizeof(tempmap));
-	}
-	else if (mapcode == 1)
-	{
-		memcpy(tempmap, map2, sizeof(tempmap));
-	}
-
-	MapDraw(&x,&y);
-
-	while (playing)
-	{
+        while (running)
+        {
+            int input = Keycontrol();
 
 
-		switch (KeyControl())
-		{
-			case UP: Move(&x, &y, 0, -1 );
-			break;
+            switch (input)
+            {
+            case UP: win = player.move(maze, 0, -1, 2); break;
+            case DOWN: win = player.move(maze, 0, 1, 2);  break;
+            case LEFT: win = player.move(maze, -1, 0, 2); break;
+            case RIGHT: win = player.move(maze, 1, 0, 2);  break;
+            }
 
-			case DOWN : Move(&x, &y,0, +1);
-			break;
+            if (win == 1)
+            {
+                gotoxy(0, Maze::rows + 3);
+                cout << "열쇠 획득";
+                Sleep(50);
 
-			case LEFT : Move(&x, &y,-2 , 0);
-			break;
+                clear++;
+            }
 
-			case RIGHT : Move(&x, &y,+2, 0);
-			break;
-		}
-	}
+            if (win == 2 && clear == 1)
+            {
+                system("cls");
+                gotoxy(0, Maze::rows + 2);
+                cout << " 탈출 성공! ";
+                running = 0;
+            }
 
-}
+        }
 
+        gotoxy(0, Maze::rows + 4);
+    }
+};
 
+// ------------------- main -------------------
 int main()
+
 {
-	 HideCursor();
-	 
-	  while (1)
-	  {
-	  	TitleDraw();
-	  
-	  	int menucode = MenuDraw();
-	  	
-	  	if (menucode == 0)
-	  	{
-			int n = maplistdraw();
-
-			if (n == 0)
-			{
-				Gloop(0);
-			}
-			else if (n == 1)
-			{
-				Gloop(1);
-			}
-	 		// MapDraw();
-			// Move();
-
-	  	}
-	  	else if (menucode == 1)
-	  	{
-	  		InfoDraw();
-	  	}
-	  	else if (menucode == 2)
-	  	{
-	  		return 0;		// return문의 기능에는 값을 반환하는것도 있지만 자신이 속해있는 함수를 종료시키는 기능도 있습니다.
-	  	}
-	  	system("cls");
-	  
-	  }
-	
-	return 0;
+    Game game;      // Game 이라는 클래스(자료형)를 사용하는 game이라는 객체를 생성하는 문구
+    game.run();     // game 이라는 객체의 run()함수를 호출
+    return 0;
 }
+
 
